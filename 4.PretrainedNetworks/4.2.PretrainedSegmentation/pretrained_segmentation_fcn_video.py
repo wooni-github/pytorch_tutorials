7d41b78b05c28bb844f2a273c3fec379a53ae523
wooni-github <dwkim.research@gmail.com>
2022-12-19 15:33:04 +0900

    Edit 3.2.LinearRegression.md

--- 4.PretrainedNetworks/4.2.PretrainedSegmentation/pretrained_segmentation_fcn_video.py
+++ 4.PretrainedNetworks/4.2.PretrainedSegmentation/pretrained_segmentation_fcn_video.py
@@ -32,17 +32,18 @@ if __name__ == '__main__':
                 start_time = time.time()
                 frame = cv2.resize(frame, (640, 360))
                 outputs = utils.get_segment_labels(frame, model, DEVICE)
-                outputs = outputs['out']  # output은 클래스에 속하는 확률이 담긴 'out'과 auxilliary loss가 담긴 'aux'로 구성되어 있음.
-
-                overlay, segmented = utils.visualize(frame, outputs)
 
                 end_time = time.time()
+
+                outputs = outputs['out']  # output은 클래스에 속하는 확률이 담긴 'out'과 auxilliary loss가 담긴 'aux'로 구성되어 있음.
+
+                overlay, segmented = utils.visualize(frame, outputs) # 가시화
+
                 fps = 1 / (end_time - start_time)
                 total_fps += fps
-
                 frame_count += 1
-
                 wait_time = max(1, int(fps / 4))
+
                 cv2.imshow('Input image', frame)
                 cv2.imshow('Overlay image', overlay)
                 cv2.imshow('Segmented image', segmented)
